0b5cb29d9ddb1e6a6acd3d566daa7f610afbfbe8
Roger Pence <roger.pence@gmail.com>
2015-08-23 22:10:39 -0500

    Add comments to refresh file

--- refresh-dot-files.sh
+++ refresh-dot-files.sh
@@ -1,3 +1,10 @@
+# Get this script with:
+# curl https://raw.githubusercontent.com/rogerpence/dotfiles/master/refresh-dot-files.sh > ~/.refresh-dot-files.sh
+
+### WARNING
+### Running this script overrides existing dot files! Be careful.
+### This script is intended to tailor new VMs.
+
 # .bash_aliases
 curl https://raw.githubusercontent.com/rogerpence/dotfiles/master/.bash_aliases > ~/.bash_aliases
 # .bash_box_aliases
