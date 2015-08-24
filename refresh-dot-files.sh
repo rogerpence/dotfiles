9b37c93b829ffd39e32ea0299daa9318aed8f177
Roger Pence <roger.pence@gmail.com>
2015-08-23 22:07:02 -0500

    Add refresh file

---
+++ refresh-dot-files.sh
@@ -0,0 +1,4 @@
+# .bash_aliases
+curl https://raw.githubusercontent.com/rogerpence/dotfiles/master/.bash_aliases > ~/.bash_aliases
+# .bash_box_aliases
+curl https://raw.githubusercontent.com/rogerpence/dotfiles/master/.bash_box_aliases ~/.bash_box_aliases
