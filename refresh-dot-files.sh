156c3773e04077e933d8c97cc19722e7c7faf861
Roger Pence <roger.pence@gmail.com>
2015-08-23 22:17:25 -0500

    fix typo in refresh file

--- refresh-dot-files.sh
+++ refresh-dot-files.sh
@@ -8,4 +8,4 @@
 # .bash_aliases
 curl https://raw.githubusercontent.com/rogerpence/dotfiles/master/.bash_aliases > ~/.bash_aliases
 # .bash_box_aliases
-curl https://raw.githubusercontent.com/rogerpence/dotfiles/master/.bash_box_aliases ~/.bash_box_aliases
+curl https://raw.githubusercontent.com/rogerpence/dotfiles/master/.bash_box_aliases > ~/.bash_box_aliases
